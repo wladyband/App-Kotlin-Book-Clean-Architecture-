f59d48ec884369ea9b7fb32d0b09e684c849c11b
Wladimir Bandeira <tecnico100@gmail.com>
2023-08-02 14:36:30 -0300

    determinando navegabilidade da tela home para a tela start

--- app/src/main/java/com/wladimirbr/readingplan/screens/home/ReaderBookHomeScreen.kt
+++ app/src/main/java/com/wladimirbr/readingplan/screens/home/ReaderBookHomeScreen.kt
@@ -1,20 +1,24 @@
 package com.wladimirbr.readingplan.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import com.wladimirbr.readingplan.components.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 import com.wladimirbr.readingplan.model.MBook
 
 import com.wladimirbr.readingplan.navigation.ReaderScreens
@@ -45,11 +49,34 @@ fun Home(
 
 @Composable
 fun HomeContent(navController: NavController) {
-    Column(Modifier.padding(2.dp),
+    val email = FirebaseAuth.getInstance().currentUser?.email
+    val currentUserName = email?.split("@")?.get(0) ?: "N/A"
+    Column(
+        Modifier.padding(2.dp),
         verticalArrangement = Arrangement.SpaceEvenly
     ) {
         Row(modifier = Modifier.align(alignment = Alignment.Start)) {
             TitleSection(label = "You  Reading \n" + " activity right... ")
+            Spacer(modifier = Modifier.fillMaxSize(0.7f))
+            Column {
+                Icon(imageVector = Icons.Filled.AccountCircle,
+                    contentDescription = "Profile",
+                    modifier = Modifier
+                        .clickable {
+                            navController.navigate(ReaderScreens.ReaderStatsScreen.name)
+                        }
+                        .size(45.dp),
+                    tint = MaterialTheme.colors.secondaryVariant)
+                Text(text = currentUserName,
+                    modifier = Modifier.padding(2.dp),
+                    style = MaterialTheme.typography.overline,
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    maxLines = 1,
+                    overflow = TextOverflow.Clip
+                )
+                Divider()
+            }
         }
     }
 }
