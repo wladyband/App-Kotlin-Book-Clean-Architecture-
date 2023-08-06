7a0497855d3b19f9a020d0dd109693d37c255dba
Wladimir Bandeira <tecnico100@gmail.com>
2023-08-06 10:08:44 -0300

    evoluindo a tela home

--- app/src/main/java/com/wladimirbr/readingplan/components/Components.kt
+++ app/src/main/java/com/wladimirbr/readingplan/components/Components.kt
@@ -1,5 +1,6 @@
 package com.wladimirbr.readingplan.components
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -7,18 +8,19 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.rounded.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -27,11 +29,16 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
+import com.wladimirbr.readingplan.model.MBook
 import com.wladimirbr.readingplan.navigation.ReaderScreens
+
 
 @Composable
 fun EmailInput(
@@ -132,6 +139,84 @@ fun FABContent(onTap: () -> Unit) {
 }
 
 
+
+
+@Preview
+@Composable
+fun ListCard(
+    book: MBook = MBook(
+        id = "dshbcjsdhb",
+        title = "Running",
+        authors = "Wladimir bandeira",
+        notes = "jsdjnksjdncjdsn"
+    ),
+    onPressDetails: (String) -> Unit = {}
+) {
+    val context = LocalContext.current
+    val resources = context.resources
+
+    val displayMetrics = resources.displayMetrics
+
+    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
+    val spacing = 10.dp
+
+    Card(shape = RoundedCornerShape(29.dp),
+        backgroundColor = Color.White,
+        elevation = 6.dp,
+        modifier = Modifier
+            .padding(16.dp)
+            .height(242.dp)
+            .width(202.dp)
+            .clickable { onPressDetails.invoke(book.title.toString()) }) {
+
+        Column(modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
+            horizontalAlignment = Alignment.Start) {
+            Row(horizontalArrangement = Arrangement.Center) {
+
+                Image(painter = rememberImagePainter(data = book.photoUrl.toString()),
+                    contentDescription = "book image",
+                    modifier = Modifier
+                        .height(140.dp)
+                        .width(100.dp)
+                        .padding(4.dp))
+                Spacer(modifier = Modifier.width(50.dp))
+
+                Column(modifier = Modifier.padding(top = 25.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(imageVector = Icons.Rounded.FavoriteBorder,
+                        contentDescription = "Fav Icon",
+                        modifier = Modifier.padding(bottom = 1.dp))
+
+                    BookRating(score = 3.4)
+                }
+
+            }
+            Text(text = book.title.toString(), modifier = Modifier.padding(4.dp),
+                fontWeight = FontWeight.Bold,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis)
+
+            Text(text = book.authors.toString(), modifier = Modifier.padding(4.dp),
+                style = MaterialTheme.typography.caption) }
+
+        val isStartedReading = remember {
+            mutableStateOf(false)
+        }
+
+        Row(horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom) {
+            isStartedReading.value = book.startedReading != null
+
+
+            RoundedButton(label = if (isStartedReading.value)  "Reading" else "Not Yet",
+                radius = 70)
+
+        }
+    }
+}
+
+
 @Composable
 fun TitleSection(modifier: Modifier = Modifier, label: String) {
     Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
@@ -145,6 +230,50 @@ fun TitleSection(modifier: Modifier = Modifier, label: String) {
         }
     }
 }
+
+@Composable
+fun RoundedButton(
+    label: String = "Reading",
+    radius: Int = 29,
+    onPress: () -> Unit = {}) {
+    Surface(modifier = Modifier.clip(RoundedCornerShape(
+        bottomEndPercent = radius,
+        topStartPercent = radius)),
+        color = Color(0xFF92CBDF)) {
+
+        Column(modifier = Modifier
+            .width(90.dp)
+            .heightIn(40.dp)
+            .clickable { onPress.invoke() },
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = label, style = TextStyle(color = Color.White,
+                fontSize = 15.sp),)
+
+        }
+    }
+}
+
+@Composable
+fun BookRating(score: Double = 4.5) {
+    Surface(
+        modifier = Modifier
+            .height(70.dp)
+            .padding(4.dp), shape = RoundedCornerShape(56.dp),
+        elevation = 6.dp,
+        color = Color.White
+    ) {
+        Column(modifier = Modifier.padding(4.dp)) {
+            Icon(
+                imageVector = Icons.Filled.StarBorder, contentDescription = "Start",
+                modifier = Modifier.padding(3.dp)
+            )
+            Text(text = score.toString(), style = MaterialTheme.typography.subtitle1)
+
+        }
+    }
+}
+
 
 @Composable
 fun InputField(
--- app/src/main/java/com/wladimirbr/readingplan/screens/home/ReaderBookHomeScreen.kt
+++ app/src/main/java/com/wladimirbr/readingplan/screens/home/ReaderBookHomeScreen.kt
@@ -3,8 +3,10 @@ package com.wladimirbr.readingplan.screens.home
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -62,6 +64,15 @@ fun Home(
 
 @Composable
 fun HomeContent(navController: NavController) {
+
+        val listOfBooks = listOf(
+          MBook(id = "dadfa", title = "Hello Again", authors = "All of us", notes = null),
+        MBook(id = "dadfa", title = " Again", authors = "All of us", notes = null),
+        MBook(id = "dadfa", title = "Hello ", authors = "The world us", notes = null),
+        MBook(id = "dadfa", title = "Hello Again", authors = "All of us", notes = null),
+        MBook(id = "dadfa", title = "Hello Again", authors = "All of us", notes = null)
+                            )
+
     val email = FirebaseAuth.getInstance().currentUser?.email
     val currentUserName = email?.split("@")?.get(0) ?: "N/A"
     Column(
@@ -93,153 +104,40 @@ fun HomeContent(navController: NavController) {
             }
 
         }
-        ListCard()
+        ReadingRightNowArea(books = listOf(), navController = navController)
+        TitleSection(label = "Reading List")
+        BoolListArea(listOfBooks = listOfBooks, navController = navController)
+
     }
 }
 
 @Composable
-fun TitleSection(modifier: Modifier = Modifier, label: String) {
-    Surface(modifier = modifier.padding(start = 5.dp, top = 1.dp)) {
-        Column {
-            Text(
-                text = label,
-                fontSize = 19.sp,
-                fontStyle = FontStyle.Normal,
-                textAlign = TextAlign.Center
-            )
-        }
+fun BoolListArea(listOfBooks: List<MBook>, navController: NavController) {
+    HorizontalScrollableComponent(listOfBooks){
+        // todo: clicar no card
     }
 }
 
-
 @Composable
-fun RoundedButton(
-    label: String = "Reading",
-    radius: Int = 29,
-    onPress: () -> Unit = {}) {
-    Surface(modifier = Modifier.clip(RoundedCornerShape(
-        bottomEndPercent = radius,
-        topStartPercent = radius)),
-        color = Color(0xFF92CBDF)) {
-
-        Column(modifier = Modifier
-            .width(90.dp)
-            .heightIn(40.dp)
-            .clickable { onPress.invoke() },
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            Text(text = label, style = TextStyle(color = Color.White,
-                fontSize = 15.sp),)
-
-        }
-
-    }
-
-
-}
-
-@Preview
-@Composable
-fun ListCard(
-    book: MBook = MBook(
-        id = "dshbcjsdhb",
-        title = "Running",
-        authors = "Wladimir bandeira",
-        notes = "jsdjnksjdncjdsn"
-    ),
-    onPressDetails: (String) -> Unit = {}
-) {
-    val context = LocalContext.current
-    val resources = context.resources
-
-    val displayMetrics = resources.displayMetrics
-
-    val screenWidth = displayMetrics.widthPixels / displayMetrics.density
-    val spacing = 10.dp
-
-    Card(shape = RoundedCornerShape(29.dp),
-        backgroundColor = Color.White,
-        elevation = 6.dp,
-        modifier = Modifier
-            .padding(16.dp)
-            .height(242.dp)
-            .width(202.dp)
-            .clickable { onPressDetails.invoke(book.title.toString()) }) {
-
-        Column(modifier = Modifier.width(screenWidth.dp - (spacing * 2)),
-            horizontalAlignment = Alignment.Start) {
-            Row(horizontalArrangement = Arrangement.Center) {
-
-                Image(painter = rememberImagePainter(data = book.photoUrl.toString()),
-                    contentDescription = "book image",
-                    modifier = Modifier
-                        .height(140.dp)
-                        .width(100.dp)
-                        .padding(4.dp))
-                Spacer(modifier = Modifier.width(50.dp))
-
-                Column(modifier = Modifier.padding(top = 25.dp),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally) {
-                    Icon(imageVector = Icons.Rounded.FavoriteBorder,
-                        contentDescription = "Fav Icon",
-                        modifier = Modifier.padding(bottom = 1.dp))
-
-                    BookRating(score = 3.4)
-                }
-
-            }
-            Text(text = book.title.toString(), modifier = Modifier.padding(4.dp),
-                fontWeight = FontWeight.Bold,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis)
-
-            Text(text = book.authors.toString(), modifier = Modifier.padding(4.dp),
-                style = MaterialTheme.typography.caption) }
-
-        val isStartedReading = remember {
-            mutableStateOf(false)
-        }
-
-        Row(horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.Bottom) {
-            isStartedReading.value = book.startedReading != null
-
-
-            RoundedButton(label = if (isStartedReading.value)  "Reading" else "Not Yet",
-                radius = 70)
-
-        }
-    }
-
-
-
-}
-
-@Composable
-fun BookRating(score: Double = 4.5) {
-    Surface(
-        modifier = Modifier
-            .height(70.dp)
-            .padding(4.dp), shape = RoundedCornerShape(56.dp),
-        elevation = 6.dp,
-        color = Color.White
+fun HorizontalScrollableComponent(listOfBooks: List<MBook>,  onCardPressed: (String) -> Unit) {
+    val scrollState = rememberScrollState()
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .height(280.dp)
+        .horizontalScroll(scrollState)
     ) {
-        Column(modifier = Modifier.padding(4.dp)) {
-            Icon(
-                imageVector = Icons.Filled.StarBorder, contentDescription = "Start",
-                modifier = Modifier.padding(3.dp)
-            )
-            Text(text = score.toString(), style = MaterialTheme.typography.subtitle1)
-
+        for (book in listOfBooks){
+            ListCard(book){
+                onCardPressed(it)
+            }
         }
     }
-
 }
+
 
 @Composable
 fun ReadingRightNowArea(books: List<MBook>, navController: NavController) {
-
+    ListCard()
 }
 
 
